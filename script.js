642b0f6472486715cf245a60d2c79d3596cc6f5d
Salvador230000913 <salvagamer007@gmail.com>
2026-05-12 11:40:44 +0100

    mudança no map pelo js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var map = L.map('map').setView([51.505, -0.09], 5);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
     maxZoom: 20,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 }).addTo(map);
 
 var marker = L.marker([51.5, -0.09]).addTo(map);
@@ -21,5 +21,10 @@ marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 circle.bindPopup("I am a circle.");
 polygon.bindPopup("I am a polygon.");
 
-map.removeControl(circle);
+map.removeLayer(circle);
 
+
+fetch("https://opensky-network.org/api/states/all")
+    .then(function(resposta){
+        console.log(resposta);
+    })
